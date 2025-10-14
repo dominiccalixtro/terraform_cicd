d0d52cd3f22000e72903cd76dc05cfb79c99ccc9
Dominic Calixtro <dominic.calixtro@gmail.com>
2025-10-14 11:26:18 +0800

    feat(terraform): add AWS infrastructure and GitHub Actions CI/CD workflow

---
+++ main.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route" "this" {
+  route_table_id         = aws_route_table.this.id
+  gateway_id             = aws_internet_gateway.this.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "this" {
+  route_table_id = aws_route_table.this.id
+  subnet_id      = aws_subnet.public_subnet.id
+}
+
+resource "aws_security_group" "this" {
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "this" {
+  ami                         = "ami-052064a798f08f0d3"
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  associate_public_ip_address  = true
+  vpc_security_group_ids       = [aws_security_group.this.id]
+
+  tags = {
+    Name = "Terraform-Test-Instance"
+  }
+}
