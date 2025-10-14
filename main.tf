e9f5be6b0665aeb6bb891792549c5a70be999a55
Dominic Calixtro <dominic.calixtro@gmail.com>
2025-10-14 12:03:00 +0800

    fix: Changed to t2.micro

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_security_group" "this" {
 
 resource "aws_instance" "this" {
   ami                         = "ami-088d74defe9802f14"
-  instance_type               = "t3.micro"
+  instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnet.id
   associate_public_ip_address  = true
   vpc_security_group_ids       = [aws_security_group.this.id]
